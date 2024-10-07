39f4ab4b790a07b7b05008c22cd6f6b42841ce7b
AmiraliRasaei <32707380+Amroach@users.noreply.github.com>
2024-10-07 19:42:14 +0100

    create a vpc

--- private-access-to-ec2/main.tf
+++ private-access-to-ec2/main.tf
@@ -11,3 +11,13 @@ terraform {
 provider "aws" {
   region = "eu-west-1"
 }
+
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
