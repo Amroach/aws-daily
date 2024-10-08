ad2b76a1c164a68ae15aca9cd452414687d527d6
AmiraliRasaei <32707380+Amroach@users.noreply.github.com>
2024-10-08 08:52:19 +0100

    deleting the vpc to create the vpc, subnet and igw together

--- private-access-to-ec2/main.tf
+++ private-access-to-ec2/main.tf
@@ -12,28 +12,4 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-# Create a VPC
-resource "aws_vpc" "ecom2_vpc" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "ecom2-vpc"
-  }
-}
-
-
-resource "aws_subnet" "ecom2_subnet_public1_eu_west_1a" {
-  vpc_id     = aws_vpc.ecom2_vpc.id
-  cidr_block = "10.0.0.0/20"
-
-  availability_zone = "eu-west-1a"
-
-  tags = {
-
-    Name = "ecom2-subnet-public1-eu-west-1a"
-  }
-}
 
