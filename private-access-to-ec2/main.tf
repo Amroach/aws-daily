8b878aeaca29840c955aefdf34b5af39ed9527b8
AmiraliRasaei <32707380+Amroach@users.noreply.github.com>
2024-10-09 20:38:15 +0100

    imported the vpc

--- private-access-to-ec2/main.tf
+++ private-access-to-ec2/main.tf
@@ -12,4 +12,12 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+# Create a VPC
+resource "aws_vpc" "ecom_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
+  tags = {
+    Name = "ecom-vpc"
+  }
+}
