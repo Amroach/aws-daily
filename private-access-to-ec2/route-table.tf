b603b15d1dc67f2e1473196492d8cabf218fc392
AmiraliRasaei <32707380+Amroach@users.noreply.github.com>
2024-10-11 22:49:17 +0100

    adding private route table

--- private-access-to-ec2/route-table.tf
+++ private-access-to-ec2/route-table.tf
@@ -3,6 +3,7 @@ resource "aws_route_table" "ecom_route_table" {
   vpc_id = aws_vpc.ecom_vpc.id
 }
 
+# public route table
 resource "aws_route_table" "ecom_rtb_public" {
   vpc_id = aws_vpc.ecom_vpc.id
 
@@ -12,6 +13,7 @@ resource "aws_route_table" "ecom_rtb_public" {
 
 }
 
+# public route 
 resource "aws_route" "ecom_rtb_public_route" {
   route_table_id = aws_route_table.ecom_rtb_public.id
 
@@ -20,6 +22,7 @@ resource "aws_route" "ecom_rtb_public_route" {
 
 }
 
+# Internet gateway
 resource "aws_internet_gateway" "ecom_igw" {
   vpc_id = aws_vpc.ecom_vpc.id
 
@@ -28,3 +31,15 @@ resource "aws_internet_gateway" "ecom_igw" {
   }
 
 }
+
+# private route table
+resource "aws_route_table" "ecom_rtb_private1_eu_west_1a" {
+  vpc_id = aws_vpc.ecom_vpc.id
+
+  tags = {
+    Name = "ecom-rtb-private1-eu-west-1a"
+  }
+}
+
+
+
