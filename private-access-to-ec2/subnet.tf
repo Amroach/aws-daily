a00b70037a2be5136973a73c2b3adb51205fc9cb
AmiraliRasaei <32707380+Amroach@users.noreply.github.com>
2024-10-13 09:11:17 +0100

    fixing the name on the subnet and creating two more subnets

--- private-access-to-ec2/subnet.tf
+++ private-access-to-ec2/subnet.tf
@@ -10,7 +10,7 @@ resource "aws_subnet" "ecom_subnet_public1_eu_west_1a" {
 }
 
 # public2 eu_west_1a subnet
-resource "aws_subnet" "ecom_subnet_public2_eu_west_1a" {
+resource "aws_subnet" "ecom_subnet_public2_eu_west_1b" {
   vpc_id     = aws_vpc.ecom_vpc.id
   cidr_block = "10.0.16.0/20"
 
@@ -18,3 +18,22 @@ resource "aws_subnet" "ecom_subnet_public2_eu_west_1a" {
     Name = "ecom-subnet-public2-eu-west-1b"
   }
 }
+
+resource "aws_subnet" "ecom_subnet_private1_eu_west_1a" {
+  vpc_id     = aws_vpc.ecom_vpc.id
+  cidr_block = "10.0.128.0/20"
+
+  tags = {
+    Name = "ecom-subnet-private1-eu-west-1a"
+  }
+}
+
+resource "aws_subnet" "ecom_subnet_private2_eu_west_1b" {
+  vpc_id     = aws_vpc.ecom_vpc.id
+  cidr_block = "10.0.144.0/20"
+
+  tags = {
+    Name = "ecom-subnet-private2-eu-west-1b"
+  }
+}
+
