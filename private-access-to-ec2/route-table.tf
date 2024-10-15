b4041b7679e1d123168ebcb89241f928be726dcf
AmiraliRasaei <rasaeiamir@gmail.com>
2024-10-15 18:26:04 +0100

    adding the private route tables for both public and private

--- private-access-to-ec2/route-table.tf
+++ private-access-to-ec2/route-table.tf
@@ -34,6 +34,18 @@ resource "aws_internet_gateway" "ecom_igw" {
   }
 }
 
+# public1_eu_west_1a route association
+resource "aws_route_table_association" "ecom_rtb_public1_eu_west_1a_association" {
+  subnet_id      = aws_subnet.ecom_subnet_public1_eu_west_1a.id
+  route_table_id = aws_route_table.ecom_rtb_public.id
+}
+
+# public2_eu_west_1b route association
+resource "aws_route_table_association" "ecom_rtb_public2_eu_west_1b_association" {
+  subnet_id      = aws_subnet.ecom_subnet_public2_eu_west_1b.id
+  route_table_id = aws_route_table.ecom_rtb_public.id
+}
+
 #-------------------------#
 # private1_eu_west_1a route table
 #-------------------------#
@@ -52,4 +64,21 @@ resource "aws_route_table_association" "ecom_rtb_private1_eu_west_1a_association
   route_table_id = aws_route_table.ecom_rtb_private1_eu_west_1a.id
 }
 
- 
+#-------------------------#
+# private2_eu_west_1b route table
+#-------------------------#
+
+resource "aws_route_table" "ecom_rtb_private2_eu_west_1b" {
+  vpc_id = aws_vpc.ecom_vpc.id
+
+  tags = {
+    Name = "ecom-rtb-private2-eu-west-1b"
+  }
+}
+
+# private2_eu_west_1b route
+resource "aws_route_table_association" "ecom_rtb_private2_eu_west_1b_association" {
+  subnet_id      = aws_subnet.ecom_subnet_private2_eu_west_1b.id
+  route_table_id = aws_route_table.ecom_rtb_private2_eu_west_1b.id
+}
+
