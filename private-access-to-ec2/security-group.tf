fd9b4c7619cbe8800aafaf10553091020f477e61
AmiraliRasaei <32707380+Amroach@users.noreply.github.com>
2024-10-19 19:24:25 +0100

    adding the security rule in

--- private-access-to-ec2/security-group.tf
+++ private-access-to-ec2/security-group.tf
@@ -3,6 +3,14 @@ resource "aws_security_group" "ecom_lab_public" {
   description = "ecom-lab-public"
   vpc_id      = aws_vpc.ecom_vpc.id
 
-  ingress = []
-  egress  = []
 }
+
+resource "aws_security_group_rule" "ecom_lab_public_egress" {
+  type              = "egress"
+  security_group_id = aws_security_group.ecom_lab_public.id
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"  # Allows all traffic
+}
+
