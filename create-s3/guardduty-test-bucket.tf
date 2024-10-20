4466b767730efca2ceeaf654e0dd0e4eafb64983
AmiraliRasaei <32707380+Amroach@users.noreply.github.com>
2024-10-20 16:04:20 +0100

    creating a test s3 bucket

---
+++ create-s3/guardduty-test-bucket.tf
@@ -0,0 +1,21 @@
+resource "aws_s3_bucket" "guardduty_test_bucket" {
+  bucket = "guardduty-test-bucket-1234567890"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    Name = "Creating test bucket for s3"
+  }
+
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket = aws_s3_bucket.guardduty_test_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
