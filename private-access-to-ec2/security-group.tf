7b60a1a7b6f972caa2824020d741f978e0e8fe16
AmiraliRasaei <32707380+Amroach@users.noreply.github.com>
2024-10-21 20:01:46 +0100

    opening to the internet

--- private-access-to-ec2/security-group.tf
+++ private-access-to-ec2/security-group.tf
@@ -23,20 +23,20 @@ resource "aws_security_group_rule" "ssh_from_ec2_instance_connect" {
   security_group_id = aws_security_group.ecom_lab_public.id
 }
 
-resource "aws_security_group_rule" "allow_http_only_from_ipv6" {
+resource "aws_security_group_rule" "allow_http" {
   type              = "ingress"
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  ipv6_cidr_blocks  = ["2a09:bac0:1000:da::155:3c/128"]
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.ecom_lab_public.id
 }
 
-resource "aws_security_group_rule" "allow_https_only_ipv6" {
+resource "aws_security_group_rule" "allow_https" {
   type              = "ingress"
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  ipv6_cidr_blocks  = ["2a09:bac0:1000:da::155:3c/128"]
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.ecom_lab_public.id
 }
