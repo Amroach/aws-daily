62963e564c31c37c6426762016123dcfdd004bf4
AmiraliRasaei <32707380+Amroach@users.noreply.github.com>
2024-10-21 19:01:54 +0100

    adding the ssh rule to access the ec2 through instant connect

--- private-access-to-ec2/security-group.tf
+++ private-access-to-ec2/security-group.tf
@@ -14,3 +14,11 @@ resource "aws_security_group_rule" "ecom_lab_public_egress" {
   protocol          = "-1" # Allows all traffic
 }
 
+resource "aws_security_group_rule" "ssh_from_ec2_instance_connect" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["18.202.216.48/29"]
+  security_group_id = aws_security_group.ecom_lab_public.id
+}
