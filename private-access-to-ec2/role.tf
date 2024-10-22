31bc24e2a7f892d197a87bee3c2d847f8cdf555b
AmiraliRasaei <32707380+Amroach@users.noreply.github.com>
2024-10-22 19:54:10 +0100

    fixing the import of the role

--- private-access-to-ec2/role.tf
+++ private-access-to-ec2/role.tf
@@ -1,5 +1,18 @@
 resource "aws_iam_role" "guardduty_s3_malware_protection" {
-  name                = "yak_role"
-  assume_role_policy  = data.aws_iam_policy_document.instance_assume_role_policy.json # (not shown)
-  managed_policy_arns = [aws_iam_policy.policy_one.arn, aws_iam_policy.policy_two.arn]
+  name        = "GuardDutyS3MalwareProtection"
+  description = "This is role used for guardDuty to be able to do malware protection on s3"
+
+  # Using jsonencode to convert the policy to valid JSON syntax
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "malware-protection-plan.guardduty.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
