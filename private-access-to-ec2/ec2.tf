1c0858acc2552f78b7d5433bbe63bcf6aae1d789
AmiraliRasaei <32707380+Amroach@users.noreply.github.com>
2024-10-26 18:46:41 +0100

    turning off the private ec2 instance

--- private-access-to-ec2/ec2.tf
+++ private-access-to-ec2/ec2.tf
@@ -1,13 +1,13 @@
-# Private EC2 instance
-resource "aws_instance" "private_ec2" {
-  ami           = "ami-054a53dca63de757b"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.ecom_subnet_private1_eu_west_1a.id
+# # Private EC2 instance
+# resource "aws_instance" "private_ec2" {
+#   ami           = "ami-054a53dca63de757b"
+#   instance_type = "t2.micro"
+#   subnet_id     = aws_subnet.ecom_subnet_private1_eu_west_1a.id
 
-  tags = {
-    Name = "private"
-  }
-}
+#   tags = {
+#     Name = "private"
+#   }
+# }
 
 # Public EC2 instance
 resource "aws_instance" "public_ec2" {
