d0ddf452b9d063a52c015f1b723f691cd99fc9a5
AmiraliRasaei <32707380+Amroach@users.noreply.github.com>
2024-10-27 10:36:27 +0000

    adding the terraform changes

---
+++ private-access-to-ec2/ebs.tf
@@ -0,0 +1,10 @@
+resource "aws_ebs_volume" "public_ec2_volume" {
+  availability_zone = "eu-west-1a"
+  size              = 8
+}
+
+resource "aws_volume_attachment" "public_ec2_volume_attachment" {
+  device_name = "/dev/xvda"
+  volume_id   = aws_ebs_volume.public_ec2_volume.id
+  instance_id = aws_instance.public_ec2.id
+}
