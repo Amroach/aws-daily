94824418b0d39dd5f85866cab82e36f5993d4590
AmiraliRasaei <32707380+Amroach@users.noreply.github.com>
2024-10-27 09:25:38 +0000

    bring back the public access

--- private-access-to-ec2/route-table.tf
+++ private-access-to-ec2/route-table.tf
@@ -7,23 +7,23 @@ resource "aws_route_table" "ecom_route_table" {
 # public route table
 #-------------------------#
 
-# resource "aws_route_table" "ecom_rtb_public" {
-#   vpc_id = aws_vpc.ecom_vpc.id
+resource "aws_route_table" "ecom_rtb_public" {
+  vpc_id = aws_vpc.ecom_vpc.id
 
-#   tags = {
-#     Name = "ecom-rtb-public"
-#   }
+  tags = {
+    Name = "ecom-rtb-public"
+  }
 
-# }
+}
 
-# # public route 
-# resource "aws_route" "ecom_rtb_public_route" {
-#   route_table_id = aws_route_table.ecom_rtb_public.id
+# public route 
+resource "aws_route" "ecom_rtb_public_route" {
+  route_table_id = aws_route_table.ecom_rtb_public.id
 
-#   destination_cidr_block = "0.0.0.0/0"
-#   gateway_id             = aws_internet_gateway.ecom_igw.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.ecom_igw.id
 
-# }
+}
 
 # Internet gateway
 resource "aws_internet_gateway" "ecom_igw" {
@@ -34,17 +34,17 @@ resource "aws_internet_gateway" "ecom_igw" {
   }
 }
 
-# # public1_eu_west_1a route association
-# resource "aws_route_table_association" "ecom_rtb_public1_eu_west_1a_association" {
-#   subnet_id      = aws_subnet.ecom_subnet_public1_eu_west_1a.id
-#   route_table_id = aws_route_table.ecom_rtb_public.id
-# }
+# public1_eu_west_1a route association
+resource "aws_route_table_association" "ecom_rtb_public1_eu_west_1a_association" {
+  subnet_id      = aws_subnet.ecom_subnet_public1_eu_west_1a.id
+  route_table_id = aws_route_table.ecom_rtb_public.id
+}
 
-# # public2_eu_west_1b route association
-# resource "aws_route_table_association" "ecom_rtb_public2_eu_west_1b_association" {
-#   subnet_id      = aws_subnet.ecom_subnet_public2_eu_west_1b.id
-#   route_table_id = aws_route_table.ecom_rtb_public.id
-# }
+# public2_eu_west_1b route association
+resource "aws_route_table_association" "ecom_rtb_public2_eu_west_1b_association" {
+  subnet_id      = aws_subnet.ecom_subnet_public2_eu_west_1b.id
+  route_table_id = aws_route_table.ecom_rtb_public.id
+}
 
 #-------------------------#
 # private1_eu_west_1a route table
